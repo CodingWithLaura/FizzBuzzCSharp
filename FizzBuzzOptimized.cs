877cd5ecb6d4ab4ac74415e9c1eaf70c9955ca67
Laura Ganss <laura.ganss@lise.de>
2021-03-23 18:43:40 +0100

    Reduce side-effects by using switch expressions

---
+++ FizzBuzzOptimized.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                string finalResult= FizzBuzz(i); 
+                Console.WriteLine(finalResult);
+            }
+            Console.Read();
+        }
+        static string FizzBuzz(int number) =>
+            number switch
+            {
+                var n when check_fizz(n) && check_buzz(n) => "fizzbuzz",
+                var n when check_buzz(n) => "buzz",
+                var n when check_fizz(n) => "fizz",
+                _ => number.ToString()
+            };
+
+        private static bool check_fizz(int zahl) => zahl % 3 == 0;
+
+        private static bool check_buzz(int zahl) => zahl % 5 == 0;
+    }
+}
